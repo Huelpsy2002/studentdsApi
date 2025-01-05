db4fa4cebe1250339cede6a06f9ea82a61247fe5
Hussein <101278998+Huelpsy2002@users.noreply.github.com>
2025-01-06 00:53:59 +0300

    Add files via upload

---
+++ bussinessLayer/Class1.cs
@@ -0,0 +1,56 @@
+﻿using dataAccesslayer;
+using sclass;
+using System.Data;
+
+namespace bussinessLayer
+{
+    public class BussinessLogic
+    {
+
+
+        public static List<sclass.Students>? getAllStudents()
+        {
+            return DataAccess.getAllStudents().Count > 0 ? DataAccess.getAllStudents() : null;
+           
+        }
+        public static sclass.Students getStudentById(int id)
+        {
+            return DataAccess.getStudentById(id);
+        }
+
+        public static List<sclass.Students>? getPassedStudents()
+        {
+            return DataAccess.getPassedStudents().Count > 0 ? DataAccess.getPassedStudents() : null;
+
+        }
+        public static sclass.Students addStudent(sclass.Students newstudent)
+        {
+            if ( string.IsNullOrEmpty(newstudent.Name) || newstudent.Age < 0 || newstudent.Grade < 0 || newstudent.Grade > 100) 
+            {
+                return null;
+            }
+                return DataAccess.AddStudent(newstudent);
+        }
+
+        public static bool deleteStudent(int id)
+        {
+            if (DataAccess.deleteStudent(id)) return true;
+            return false;
+        }
+
+        public static sclass.Students updateStudent(sclass.Students student)
+        {
+            if (string.IsNullOrEmpty(student.Name) || student.Age < 0 || student.Grade < 0 || student.Grade > 100)
+            {
+                return null;
+            }
+            var updatedStudent = DataAccess.updateStudent(student);
+            if (updatedStudent != null)
+            {
+                return updatedStudent;
+            }
+            return null;
+        }
+
+    }
+}
